86570c9166f9c9d266c0caa423f7377c12bcce39
Sayantan-12 <110693598+Sayantan-12@users.noreply.github.com>
2022-08-13 18:51:07 +0530

    Hover effect added and navbar updated

---
+++ APC_Alumni_ProjectWork-main/APC_Alumni_ProjectWork-main/APC_Alumni_Project/js/utility.js
@@ -0,0 +1,17 @@
+const sumanp =document.getElementById("suman");
+const sumanimg=document.getElementById("sumanimg")
+function maximise(){
+    sumanp.style.display="block";
+    sumanp.style.marginTop="50px";
+    sumanimg.style.width="95px";
+    sumanimg.style.height="95px";
+}
+
+function minimise(){
+    sumanp.style.display="none";
+    sumanimg.style.width="200px";
+    sumanimg.style.height="200px";
+}
+// function {
+
+// }
