536059df9f3b64241a998c81d1b3794ad66f598a
writzzzz <foxtrotweco5@gmail.com>
2022-08-17 23:59:46 +0530

    max min problem

--- js/utility.js
+++ js/utility.js
@@ -1,15 +1,87 @@
+const akashp =document.getElementById("akash");
+const akashimg=document.getElementById("akashimg");
+function max11(){
+    akashp.style.display="block";
+    akashp.style.marginTop="50px";
+    akashimg.style.width="113px";
+    akashimg.style.height="113px";
+}
+function min11(){
+    akashp.style.display="none";
+    akashimg.style.width="200px";
+    akashimg.style.height="200px";
+}
+
 const sumanp =document.getElementById("suman");
-const sumanimg=document.getElementById("sumanimg")
-// function maximise(){
-    // sumanp.style.display="block";
-    // sumanp.style.marginTop="50px";
-    // sumanimg.style.width="95px";
-    // sumanimg.style.height="95px";
+const sumanimg=document.getElementById("sumanimg");
+function max22(){
+    sumanp.style.display="block";
+    sumanp.style.marginTop="50px";
+    sumanimg.style.width="113px";
+    sumanimg.style.height="113px";
+}
+function min22(){
+    sumanp.style.display="none";
+    sumanimg.style.width="200px";
+    sumanimg.style.height="200px";
+}
 
-// }
+const sayantanp =document.getElementById("sayantan");
+const sayantanimg=document.getElementById("sayantanimg");
+function max33(){
+    sayantanp.style.display="block";
+    sayantanp.style.marginTop="50px";
+    sayantanimg.style.width="113px";
+    sayantanimg.style.height="113px";
+}
+function min33(){
+    sayantanp.style.display="none";
+    sayantanimg.style.width="200px";
+    sayantanimg.style.height="200px";
+}
 
-// function minimise(){
-    // sumanp.style.display="none";
-    // sumanimg.style.width="200px";
-    // sumanimg.style.height="200px";
-// }
+const writabratap =document.getElementById("writabrata");
+const writabrataimg=document.getElementById("writabrataimg");
+function max44(){
+    writabratap.style.display="block";
+    writabratap.style.marginTop="50px";
+    writabrataimg.style.width="113px";
+    writabrataimg.style.height="113px";
+    console.log("writs max alright");
+}
+function min44(){
+    writabratap.style.display="none";
+    writabrataimg.style.width="200px";
+    writabrataimg.style.height="200px";
+    console.log("writs min alright");
+}
+
+const hrithikp =document.getElementById("hrithik");
+const hrithikimg=document.getElementById("hrithikimg");
+function max66(){
+    hrithikp.style.display="block";
+    hrithikp.style.marginTop="50px";
+    hrithikimg.style.width="113px";
+    hrithikimg.style.height="113px";
+}
+function min66(){
+    hrithikp.style.display="none";
+    hrithikimg.style.width="200px";
+    hrithikimg.style.height="200px";
+}
+
+const gauravp =document.getElementById("gaurav");
+const gauravimg=document.getElementById("gauravimg");
+function max55(){
+    gauravp.style.display="block";
+    gauravp.style.marginTop="50px";
+    gauravimg.style.width="113px";
+    gauravimg.style.height="113px";
+    console.log("grv max alright");
+}
+function min55(){
+    gauravp.style.display="none";
+    gauravimg.style.width="200px";
+    gauravimg.style.height="200px";
+    console.log("grv min alright");
+}
