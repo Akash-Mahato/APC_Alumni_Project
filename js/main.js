e23adc9fa2b25a06e66cfeb7c7e5ecaa77857e89
Sayantan-12 <110693598+Sayantan-12@users.noreply.github.com>
2022-08-21 20:58:52 +0530

    Home page fix

--- js/main.js
+++ js/main.js
@@ -51,10 +51,10 @@
     center: true,
     responsive: {
       0: {
-        items: 1,
+        items: 2,
       },
       576: {
-        items: 1,
+        items: 2,
       },
       768: {
         items: 2,
